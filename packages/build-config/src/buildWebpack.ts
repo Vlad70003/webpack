efec53b39d1e324aea0bfcf841036c2e877f6b18
vlad <vporokhin@corp.local>
2024-03-26 17:08:31 +0300

    fix

---
+++ packages/build-config/src/buildWebpack.ts
@@ -0,0 +1,30 @@
+import webpack from "webpack";
+import path from "path";
+import {buildDevServer} from "./buildDevServer";
+import {buildLoaders} from "./buildLoaders";
+import {buildPlugins} from "./buildPlugins";
+import {buildResolves} from "./buildResolves";
+import {BuildOptions} from "./types/types";
+
+export function buildWebpack(options: BuildOptions): webpack.Configuration {
+
+    const {mode, paths} = options
+    const isDev = options.mode === 'development';
+
+    return {
+        mode: mode ?? 'development',
+        entry: paths.entry,
+        output: {
+            path: paths.output,
+            filename: '[name].[contenthash].js',
+            clean: true,
+        },
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolves(options),
+        devServer: isDev ? buildDevServer(options) : undefined,
+        devtool: isDev ? 'inline-source-map' : false,
+        plugins: buildPlugins(options),
+    }
+}
