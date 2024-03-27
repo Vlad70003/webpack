7ca413bb1b8acfc60d14ba245ec98abae8f03ef0
vlad <vporokhin@corp.local>
2024-03-27 16:52:48 +0300

    fix

---
+++ services/host/src/declarations.d.ts
@@ -0,0 +1,12 @@
+declare module "*.module.scss" {
+    const content: {[className: string]: string};
+    export = content;
+}
+
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+
+declare const __PLATFORM__: 'mobile' | 'desktop';
---
+++ services/host/webpack.config.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+import webpack from 'webpack';
+import {BuildMode, BuildPaths, BuildPlatform, buildWebpack} from '@packages/build-config';
+
+interface EnvVariables {
+    mode?: BuildMode,
+    port?: number,
+    analyzer?: boolean,
+    platform?: BuildPlatform,
+}
+
+export default (env: EnvVariables) => {
+
+    const paths: BuildPaths = {
+        output: path.resolve(__dirname, 'build'),
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+        src: path.resolve(__dirname, 'src'),
+        public: path.resolve(__dirname, 'public')
+    }
+
+    const config: webpack.Configuration = buildWebpack({
+        port: env.port ?? 3000,
+        mode: env.mode ?? 'development',
+        paths: paths,
+        analyzer: env.analyzer,
+        platform: env.platform ?? 'desktop',
+    })
+
+    return config;
+};
